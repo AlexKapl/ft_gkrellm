0cd2561c04dc8cad3c583eee28bcbde944bbd0ce
Heorhii Shakula <hshakula@e1r8p7.unit.ua>
2017-11-11 21:54:55 +0200

    fastcommit

--- Kernel.cpp
+++ Kernel.cpp
@@ -14,16 +14,23 @@
 
 // * STATICS **************************************************************** //
 
-std::string Kernel::getKernelInfoByName(const std::string &name) {
+std::string Kernel::getKernelInfo(const std::string &name) {
 	char info[1024];
 	size_t size = sizeof(info);
-//	char *str = new char[name.size() + 1];
 
-//	std::strcpy(str, name.c_str());
 	sysctlbyname(name.c_str(), &info, &size, NULL, 1024);
 	std::string ret = info;
-
 	return (ret);
+}
+
+std::string Kernel::getKernelInfoInt(const std::string &name) {
+	std::stringstream ss;
+	unsigned int ret;
+	size_t size = sizeof(ret);
+
+	sysctlbyname(name.c_str(), &ret, &size, NULL, 1024);
+	ss << ret;
+	return (ss.str());
 }
 
 std::string Kernel::getUserName() {
@@ -35,6 +42,34 @@ std::string Kernel::getUserName() {
 		name = pwuid->pw_name;
 	}
 	return (name);
+}
+
+std::string	Kernel::getCoreInfo(int core) {
+	std::stringstream ss;
+	unsigned int core_count;
+	unsigned long long used, total;
+	processor_cpu_load_info_t cpuInfo;
+	mach_msg_type_number_t cpu_msg_count;
+
+	int rc =  host_processor_info(
+			mach_host_self(),
+			PROCESSOR_CPU_LOAD_INFO,
+			&core_count,
+			reinterpret_cast<processor_info_array_t *>(&cpuInfo),
+			&cpu_msg_count
+	);
+	if (rc != 0)
+		throw std::runtime_error("Error: failed to scan processor info");
+
+	if (core < 0 || static_cast<int>(core_count) <= core)
+		throw std::runtime_error("Error: invalid core number");
+	used =  cpuInfo[core].cpu_ticks[CPU_STATE_USER];
+	used += cpuInfo[core].cpu_ticks[CPU_STATE_NICE];
+	used += cpuInfo[core].cpu_ticks[CPU_STATE_SYSTEM];
+	total = used + cpuInfo[core].cpu_ticks[CPU_STATE_IDLE];
+//	ss << std::setprecision(2) << std::setiosflags(std::ios_base::fixed);
+	ss << (static_cast<double>(used) / static_cast<double >(total) * 100.0);
+	return (ss.str());
 }
 
 // * CONSTRUCTORS *********************************************************** //
